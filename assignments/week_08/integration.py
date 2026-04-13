300b6997ad64982514d5b7b6e0e9520f4943a0e4
Braden Patterson <pbraden48@gmail.com>
2026-04-13 10:27:21 -0400

    sync

--- assignments/week_08/integration.py
+++ assignments/week_08/integration.py
@@ -2,11 +2,33 @@
 # this module will contain functions for numerical integration
 
 
-def trapezoidal_rule():
-    #write the arguments, docstring, and actual code of this function
+def trapezoidal_rule(f, a, b, N):
 
-    return
+    """
+    Approximates the integral of a function f from a to b using the trapezoidal rule.
+    
+    parameters: 
+    f: function
+    The function to be integrated.
+    def trapezoidal_rule(f, a, b, N):
+    a: float
+    The lower limit of integration.
+    b: float
+    The upper limit of integration.
+    N: int
+    The number of slices
 
+    Returns:
+    float
+    The approximate value of the integral
+    """
+    
+    h = (b - a) / N
+    total = 0.5 * f(a) + 0.5 * f(b)
+    for k in range(1, N):
+        s += f(a + k * h)
+ 
+    return h * s
 
 
 def simpsons_rule():
